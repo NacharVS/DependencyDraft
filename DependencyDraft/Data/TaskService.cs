c72632fa1f69b7b30e8654e00578d1d044e9b0fe
nacharvs <r3dinc@yandex.ru>
2022-05-12 06:27:52 +0300

    start

---
+++ DependencyDraft/Data/TaskService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyDraft.Data
+{
+    public class TaskService
+    {
+        //public List<TaskService> listOfTasks = GetSimpleList();
+        public TaskService()
+        {
+        }
+
+        public TaskService(string taskName, string nameOfColoborator)
+        {
+            TaskName = taskName;
+            NameOfColoborator = nameOfColoborator;
+            InProgress = false;
+            IsDone = false;
+        }
+
+        public string TaskName { get; set; }
+        public string NameOfColoborator { get; set; }
+        public bool InProgress { get; set; }
+        public bool IsDone { get; set; }
+
+
+
+        public List<TaskService> GetSimpleList()
+        {
+            var list = new List<TaskService>()
+            {
+                new TaskService()
+                {
+                    TaskName = "Buy Products",
+                    NameOfColoborator = "Ivan",
+                },
+                new TaskService()
+                {
+                    TaskName = "Wash Car",
+                    NameOfColoborator = "Egor",
+                },
+                new TaskService()
+                {
+                    TaskName = "Make a cake",
+                    NameOfColoborator = "Alice",
+                }
+            };
+
+            return list;
+        }
+
+        public void AddToList(List<TaskService> list, TaskService item)
+        {
+            list.Add(item);
+
+        }
+
+    }
+}
---
+++ DependencyDraft/Shared/TaskTable.razor.cs
@@ -0,0 +1,17 @@
+﻿using DependencyDraft.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyDraft.Shared
+{
+    public partial class TaskTable
+    {
+        [Inject]
+        TaskService taskService { get; set; }
+
+        
+    }
+}
